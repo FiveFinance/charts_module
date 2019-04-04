976ae553c96ef0418a5438b6c523b847b33573ca
Marcos Da Silva <dasilva.marcos@gmail.com>
2019-04-04 02:32:26 -0700

    added LineChart component renders SVG graph from props fetched LineChart uses: 	 Min/Max X & Y Helper Functions 	 SVG Coordinate Helper To create and return an SVG element Path, an Axis and a Line Graph

--- client/src/LineChart.js
+++ client/src/LineChart.js
@@ -1,10 +1,96 @@
-import React, { Component } from 'react';
+import React from "react"
+import "./LineChart.css"
 
-class LineChart extends Component {
-
-
-  render() {
-    return <div> {this.props.data}
-          </div>;
+class LineChart extends React.Component {
+    // GET MAX & MIN X
+    getMinX() {
+      const {data} = this.props;
+      return data[0].x;
+    }
+    getMaxX() {
+      const {data} = this.props;
+      return data[data.length - 1].x;
+    }
+    // GET MAX & MIN Y
+    getMinY() {
+      const {data} = this.props;
+      return data.reduce((min, p) => p.y < min ? p.y : min, data[0].y);
+    }
+    getMaxY() {
+      const {data} = this.props;
+      return data.reduce((max, p) => p.y > max ? p.y : max, data[0].y);
+    }
+    // GET SVG COORDINATES
+    getSvgX(x) {
+      const {svgWidth} = this.props;
+      return (x / this.getMaxX() * svgWidth);
+    }
+    getSvgY(y) {
+      const {svgHeight} = this.props;
+      return svgHeight - (y / this.getMaxY() * svgHeight);
+    }
+    // BUILD SVG PATH
+    makePath() {
+      const {data, color} = this.props;
+      let pathD = "M " + this.getSvgX(data[0].x) + " " + this.getSvgY(data[0].y) + " ";
+  
+      pathD += data.map((point, i) => {
+        return "L " + this.getSvgX(point.x) + " " + this.getSvgY(point.y) + " ";
+      });
+  
+      return (
+        <path className="linechart_path" d={pathD} style={{stroke: color}} />
+      );
+    }
+    // BUILD GRID AXIS
+    makeAxis() {
+    const minX = this.getMinX(), maxX = this.getMaxX();
+    const minY = this.getMinY(), maxY = this.getMaxY();
+  
+    return (
+      <g className="linechart_axis">
+        <line
+          x1={this.getSvgX(minX)} y1={this.getSvgY(minY)}
+          x2={this.getSvgX(maxX)} y2={this.getSvgY(minY)} />
+        <line
+          x1={this.getSvgX(minX)} y1={this.getSvgY(minY)}
+          x2={this.getSvgX(minX)} y2={this.getSvgY(maxY)} />
+      </g>
+      );
+    }
+      makeArea() {
+      const {data} = this.props;
+      let pathD =
+        "M " + this.getSvgX(data[0].x) + " " + this.getSvgY(data[0].y) + " ";
+  
+      data.map((point, i) => {
+        pathD += "L " + this.getSvgX(point.x) + " " + this.getSvgY(point.y) + " ";
+      });
+  
+      pathD += "L " + this.getSvgX(this.getMaxX()) + " " + this.getSvgY(this.getMinY()) + " "
+      + "L " + this.getSvgX(this.getMinX()) + " " + this.getSvgY(this.getMinY()) + " ";
+  
+      return <path className="linechart_area" d={pathD} />
+    }
+    // RENDER & RETURN SVG PATH AND AXIS
+    render() {
+      const {svgHeight, svgWidth} = this.props;
+  
+      return (
+        <svg viewBox={`0 0 ${svgWidth} ${svgHeight}`}>
+          {this.makePath()}
+          {this.makeArea()}
+          {this.makeAxis()}
+        </svg>
+      );
+    }
   }
-}
+  // DEFAULT PROPS
+  LineChart.defaultProps = {
+    data: [],
+    color: '#1ae9aa',
+    svgHeight: 300,
+    svgWidth: 700
+  }
+
+export default LineChart;
