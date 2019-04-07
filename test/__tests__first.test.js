29ef622e997ca4f4cd7fbd639d3b209518592703
Marcos Da Silva <dasilva.marcos@gmail.com>
2019-04-06 21:08:19 -0700

    add simple unit tests test with Jest and Supertest

---
+++ test/__tests__first.test.js
@@ -0,0 +1,17 @@
+const letterCount = require("./")
+
+test("letterCount words with regular strings", () => {
+    expect(letterCount("awesome", "e")).toBe(2);
+});
+
+test("arithmetic", function() {
+    expect(4 + 4).toBeGreaterThan(7);
+    expect(4 + 4).toBeLessThan(9);
+  });
+  
+  test("references", function() {
+    var arr = [1, 2, 3];
+    expect(arr).toEqual([1, 2, 3]);
+    expect(arr).not.toBe([1, 2, 3]); // since === doesn't do deep comparison!
+    expect(arr).toContain(1);
+  });
