3c481cac8d2eaa0efca8d6a5f9c2d6d832c3ace0
Marcos Da Silva <dasilva.marcos@gmail.com>
2019-04-08 16:43:33 -0700

    added Puppeteer and Jest for testing added HoverTip draws circles onHover

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import LineChart from './LineChart';
+import ToolTip from './ToolTip';
 
-export default class FetchStockQuotes extends Component {
-  
-  state = {
+class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
     loading: true,
-    data: null
+    data: null,
+    hoverLoc: null,
+    activePoint: null
   };
+}
+
+  handleChartHover(hoverLoc, activePoint){
+    this.setState({
+      hoverLoc: hoverLoc,
+      activePoint: activePoint
+    })
+  }
 
   async componentDidMount(){
     const url = 'http://localhost:3000/AAPL';
@@ -22,10 +34,14 @@ export default class FetchStockQuotes extends Component {
   render() {
     return (
       <div className="App">
-        <div className="header"> Apple</div>
-        <div className="header"> $188.47</div>
-        {this.state.data && <LineChart data={this.state.data}/>} 
+      <div className="header"> Apple</div>
+      <div className="header"> $188.47</div>
+                    { !this.state.loading ?
+              <LineChart data={this.state.data} onChartHover={ (a,b) => this.handleChartHover(a,b) }/>
+              : null }
       </div>
     );
   }
 }
+
+export default App;
